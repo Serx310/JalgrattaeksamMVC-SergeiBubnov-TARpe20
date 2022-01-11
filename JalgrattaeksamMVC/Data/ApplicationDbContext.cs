21f3180d1142e808ffbb30839ceffb3d349b19ab
Serx310 <sergeibubnov120@gmail.com>
2022-01-11 14:12:20 +0200

    Lisa registreerimise, teooria, slaalomi, ringi ja tänava

---
+++ JalgrattaeksamMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JalgrattaeksamMVC.Models;
+
+namespace JalgrattaeksamMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Eksam>()
+                .HasIndex(e => new { e.Eesnimi, e.Perenimi })
+                .IsUnique();
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JalgrattaeksamMVC.Models.Eksam> Eksam { get; set; }
+    }
+}
